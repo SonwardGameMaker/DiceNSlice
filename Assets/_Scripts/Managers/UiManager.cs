99536993788695fae53a1f7e811abc03f865ff7a
SonwardGameMaker <sonward.gamemaker@gmail.com>
2025-01-04 19:51:13 +0200

    Quick Changes

--- Assets/_Scripts/Managers/UiManager.cs
+++ Assets/_Scripts/Managers/UiManager.cs
@@ -68,6 +68,7 @@ public class UiManager : MonoBehaviour
         GameObject go = Instantiate(prefab, Vector3.zero, Quaternion.identity);
         go.transform.SetParent(characterGroup.transform); // test if this work properly
         go.GetComponent<CharacterFrame>().Setup(character);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(characterGroup.GetComponent<RectTransform>());
     }
 
     public bool RemoveCharacter(Character character)
